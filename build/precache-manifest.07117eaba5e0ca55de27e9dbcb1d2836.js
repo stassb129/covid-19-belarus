e320bb6e2e7ad45525cab38ad5fc5b9d3f502b4b
stassb129 <45415961+stassb129@users.noreply.github.com>
2020-04-23 03:40:02 +0300

    Add files via upload

---
+++ build/precache-manifest.07117eaba5e0ca55de27e9dbcb1d2836.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "674cd07448f26e2908c1a933cccd1e0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e309a56b1acb8f528b4",
+    "url": "/static/css/main.a5574315.chunk.css"
+  },
+  {
+    "revision": "76d143b5d7e1efec8327",
+    "url": "/static/js/2.2fae53dc.chunk.js"
+  },
+  {
+    "revision": "77c52c67682ebaf3c87f021fd687577f",
+    "url": "/static/js/2.2fae53dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e309a56b1acb8f528b4",
+    "url": "/static/js/main.6080b85e.chunk.js"
+  },
+  {
+    "revision": "c67e7fc494555f284ce1",
+    "url": "/static/js/runtime-main.a43b6eb2.js"
+  },
+  {
+    "revision": "ecc00413e8c5212c91aeaa3b3b7f2de2",
+    "url": "/static/media/med.ecc00413.png"
+  }
+]);
